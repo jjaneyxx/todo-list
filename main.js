06392ed789a4923f5008b7830d05198bf486e1dd
imjjaney <hmyeonge@naver.com>
2024-07-04 09:07:15 +0900

    tab 에 따라 보여지는 화면 구현

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let deleteButton = document.getElementById("delete-all");
+let tabs = document.querySelectorAll(".tab-area div"); // tab-area 밑 div 를 모두 가져옴
+let mode = "all";
 let taskList = [];
+let filterList = [];
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
 addButton.addEventListener("click", addTask);
 deleteButton.addEventListener("click", deleteAll);
@@ -33,24 +42,31 @@ function randomIDGenerate() {
 }
 
 function render() {
+  let list = []; // 상황에 따라 값이 변하는 리스트
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "proceeding" || mode === "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete === true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete === true) {
       // task 가 완료되었다면
       resultHTML += `<div class="task-grey">
-          <div class = "task-done">${taskList[i].taskContent}</div>
+          <div class = "task-done">${list[i].taskContent}</div>
           <div class="button-area">
-            <button onclick = "itemComplete('${taskList[i].id}')" class = "done-button"><i class="fa-solid fa-arrow-rotate-right"></i></button>
-            <button onclick = "deleteItem('${taskList[i].id}')" class = "delete-button"><i class="fa-solid fa-trash"></i></button>  
+            <button onclick = "itemComplete('${list[i].id}')" class = "done-button"><i class="fa-solid fa-arrow-rotate-right"></i></button>
+            <button onclick = "deleteItem('${list[i].id}')" class = "delete-button"><i class="fa-solid fa-trash"></i></button>  
           </div>
         </div>`;
     } else {
       // task 가 완료되지 않음
       resultHTML += `<div class="task">
-    <div>${taskList[i].taskContent}</div>
+    <div>${list[i].taskContent}</div>
     <div class="button-area">
-      <button onclick = "itemComplete('${taskList[i].id}')" class = "done-button"><i class="fa-solid fa-check"></i></button>
-      <button onclick = "deleteItem('${taskList[i].id}')" class = "delete-button"><i class="fa-solid fa-trash"></i></button>  
+      <button onclick = "itemComplete('${list[i].id}')" class = "done-button"><i class="fa-solid fa-check"></i></button>
+      <button onclick = "deleteItem('${list[i].id}')" class = "delete-button"><i class="fa-solid fa-trash"></i></button>  
     </div>
   </div>`;
     }
@@ -84,4 +100,35 @@ function deleteItem(id) {
 function deleteAll() {
   taskList = [];
   render();
+}
+
+function filter(event) {
+  console.log("filter", event.target.id);
+  mode = event.target.id;
+  filterList = [];
+  doneList = [];
+  if (mode === "all") {
+    // 전체 리스트를 보여줌
+    // 기존에 만든 내용을 render() 로 불러옴
+    render();
+  } else if (mode === "proceeding") {
+    // task.isComplete === false
+    // 진행되어야 하는 아이템
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+        console.log(filterList);
+      }
+    }
+    render();
+  } else if (mode === "done") {
+    // task.isComplete === true
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+        console.log(filterList);
+      }
+    }
+    render();
+  }
 }
